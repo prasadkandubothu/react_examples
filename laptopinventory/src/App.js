27e29d261856b155105106b3aa72af6b008c7d42
Prasad Kandubothu <Prasad_Kandubothu@epam.com>
2019-12-30 17:49:54 +0530

    laptopinventory

---
+++ laptopinventory/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './header/header'
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+     Test
+    </div>
+  );
+}
+
+export default App;
