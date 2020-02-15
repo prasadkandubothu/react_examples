3aeb5e8f98345fbc353e8fa4f1c755195dfc2a89
prasadkandubothu <prasad.kandubothu@outlook.com>
2020-02-15 09:47:15 +0530

    redux

--- react_practice/src/reduxdemo2/components/Cake.js
+++ react_practice/src/reduxdemo2/components/Cake.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { buyCake } from '../redux/cake/actions-cake';
+import { connect } from 'react-redux';
 
-export default class Cake extends Component {
+class Cake extends Component {
     render() {
         console.log(this.props);
         return (
             <div>
-                Number of Cakes Available : 
+                Number of Cakes Available : {this.props.numOfCakes}
                 <br/>
-                <button>Buy a cake</button>
+                <button onClick={this.props.buyCake}>Buy a cake</button>
             </div>
         )
     }
+
 }
+
+
+const mapStateToProps = (state) => {
+        return {
+            numOfCakes : state.numOfCakes
+        }
+}
+
+const mapDispathcToState = (dispatch) => {
+    return {
+        buyCake : () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispathcToState)(Cake)
